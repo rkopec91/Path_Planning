0981816f8e56f397a07752723a4d65e94ba2d5f1
Ryan Kopec <ryankopec91@gmail.com>
2019-12-03 21:47:58 -0500

    Add spline.h file.

--- src/main.cpp
+++ src/main.cpp
@@ -7,6 +7,7 @@
 #include "Eigen-3.3/Eigen/QR"
 #include "helpers.h"
 #include "json.hpp"
+#include "spline.h"
 
 // for convenience
 using nlohmann::json;
@@ -97,6 +98,8 @@ int main() {
            * TODO: define a path made up of (x,y) points that the car will visit
            *   sequentially every .02 seconds
            */
+
+          
 
 
           msgJson["next_x"] = next_x_vals;
